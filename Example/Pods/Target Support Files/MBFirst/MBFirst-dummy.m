ccb51c8b876974b8a4b14064f55daa9905504960
Kishan Nakum <nakum84@gmail.com>
2017-05-10 09:24:24 +0530

    first

---
+++ Example/Pods/Target Support Files/MBFirst/MBFirst-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MBFirst : NSObject
+@end
+@implementation PodsDummy_MBFirst
+@end
---
+++ MBFirst/Classes/File.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  Pods
+//
+//  Created by Kishan Nakum on 10/05/17.
+//
+//
+
+import Foundation
+import UIKit
+
+
+class mbUtility{
+    
+    
+    func getColor(r:CGFloat,g:CGFloat,b:CGFloat)->UIColor{
+        let color =  UIColor(red: r/255, green: g/255, blue: b/255, alpha: 1)
+        return color
+    }
+    
+    func showAlert(message:String,controller:UIViewController){
+        
+        let alert = UIAlertController(title: "Demo", message: message, preferredStyle: .alert)
+        
+        controller.present(alert, animated: true, completion: nil)
+    }
+    
+    
+}
